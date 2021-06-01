92c72374f812c1900302b32b7abd5f66573cba0a
LiChao7457 <lichao7457@gmail.com>
2021-06-01 18:02:18 +0800

    init 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# jwt
+# 安装
+
+## npm
+
+```shell script
+npm install -S @jciedu/jwt
+```
+
+## yarn
+
+```shell script
+yarn add @jciedu/jwt
+```
+
+# 使用
+
+```js
+import {signature, has_signed, verify_signed, parse, encode, decode, assemble, disassemble} from '@jciedu/jwt'
+// 签名
+signature(header, payload, salt, alg = '')
+// 是否签名
+has_signed(token)
+// 验证签名
+verify_signed(token, salt, alg = '')
+// 解析token数据
+parse(token)
+// 编码
+encode(arr)
+// 解码
+decode(str)
+// 组装
+assemble(header, payload, sign)
+// 分解
+disassemble(token)
+
+```
---
+++ index.js
@@ -0,0 +1,102 @@
+import {Base64} from "js-base64";
+import hs256 from 'crypto-js/hmac-sha256'
+import sha1 from 'crypto-js/sha1'
+
+/**
+ * 数据签名
+ * @param header
+ * @param payload
+ * @param salt
+ * @param alg
+ * @returns {*}
+ */
+export function signature(header, payload, salt, alg = '') {
+    const plaintext = header + '.' + payload;
+    switch (alg.toLowerCase()) {
+        case 'hs256':
+            return hs256(plaintext, salt).toString();
+        default:
+            return sha1(plaintext).toString();
+    }
+}
+
+/**
+ * 是否签名
+ * @param token
+ * @returns {boolean}
+ */
+export function has_signed(token) {
+    const arr = token.split('.')
+    return arr.length === 3
+}
+
+/**
+ * 验证签名
+ * @param token
+ * @param salt
+ * @param alg
+ * @returns {boolean}
+ */
+export function verify_signed(token, salt, alg = '') {
+    let [header, payload, sign] = disassemble(token);
+    return signature(header, payload, sign, alg) === sign;
+}
+
+/**
+ * 解析token数据
+ * @param token
+ * @returns {any}
+ */
+export function parse(token) {
+    let [header, payload, sign] = disassemble(token);
+    const h = decode(header);
+    if (payload === null && sign === null) {
+        return h;
+    } else {
+        const p = decode(payload);
+        return Object.assign(h, p);
+    }
+}
+
+/**
+ * 编码
+ * @param arr
+ * @returns {string}
+ */
+export function encode(arr) {
+    return Base64.encodeURI(JSON.stringify(arr))
+}
+
+/**
+ * 解码
+ * @param str
+ * @returns {any}
+ */
+export function decode(str) {
+    return JSON.parse(Base64.decode(str))
+}
+
+/**
+ * 组装
+ * @param header
+ * @param payload
+ * @param sign
+ * @returns {string}
+ */
+export function assemble(header, payload, sign) {
+    return header + '.' + payload + '.' + sign
+}
+
+/**
+ * 分解
+ * @param token
+ * @returns {*[]|*|string[]}
+ */
+export function disassemble(token) {
+    const arr = token.split('.')
+    if (arr.length === 3) {
+        return arr;
+    }
+    return [arr[0], null, null];
+}
+
